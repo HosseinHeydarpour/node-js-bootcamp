c9b77fa7a1203270b576b87354209186cfd9f247
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-01 23:03:40 +0330

    Reading and Writing Files

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const fs = require("fs");
 
-const hello = "Hello world";
-console.log(hello);
+const textInput = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textInput);
+
+const textOutput = `This is what we know about the avacado: ${textInput}.\nCreated on ${Date.now()}`;
+
+// This does not return anythong meaningful
+fs.writeFileSync("./txt/output.txt", textOutput);
+console.log("FILE WRITTEN");
