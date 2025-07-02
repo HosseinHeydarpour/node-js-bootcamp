fe8439d2221f8cc305db5f12b2393d24fc9a2380
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-02 10:23:12 +0330

    Creating a Simple Web Server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const http = require("http"); // This gives us networking capabilities
 
 // Blocking, synchronous way
 
@@ -17,20 +18,34 @@ const fs = require("fs");
 
 // Non-blocking, asynchronous way
 // This triangle is called callback hell
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) {
-    return console.log("ERROR 😡");
-  }
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) {
+//     return console.log("ERROR 😡");
+//   }
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("FILE HAS BEEN WRITTEN 😁");
-      });
-    });
-  });
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("FILE HAS BEEN WRITTEN 😁");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file");
+
+// ************************************************************
+// * Creating a Simple Web Server
+// ************************************************************
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  // Sending back a very simple response for a request
+  res.end("REQUEST WAS SUCCESSFULL");
 });
-console.log("Will read file");
+
+// Params: Port, Host: deafults to local host
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server ready and listening to requests on port 8000");
+});
