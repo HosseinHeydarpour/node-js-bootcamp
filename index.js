9c2bd66946d1b6b2f7ccd17c72d5ee36b28f0d45
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-03 13:31:50 +0330

    Using Modules 2: Our Own Modules

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const http = require("http"); // This gives us networking capabilities
 const url = require("url");
+// . means the current location of the module
+const replaceTemplate = require("./modules/replaceTemplate");
 
 // Blocking, synchronous way
 
@@ -233,22 +235,6 @@ const url = require("url");
 // ************************************************************
 // * Parsing Variables from URLs
 // ************************************************************
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  }
-  output = output.replace(/{%ID%}/g, product.id);
-
-  return output;
-};
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
