b2f975b06dd6fd4ba6b4976c2c5670a7a64967be
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-04 20:12:05 +0330

    Building Promises

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const fs = require('fs');
 const http = require('http');
+const { resolve } = require('path');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/product.txt`, 'utf-8', (err, data) => {
-  console.log('Product: ', data);
-  superagent
-    .get(`https://fakestoreapi.com/products/${data}`)
-    .then((res) => {
-      console.log(res.body.image);
-
-      fs.writeFile(`${__dirname}/product-image.txt`, res.body.image, (err) => {
-        if (err) {
-          console.error('Error writing product image to file:', err);
-        } else {
-          console.log('Product image saved to product-image.txt');
-        }
-      });
-    })
-    .catch((err) => {
-      console.error('Error writing product image to file:', err);
+const readFilePro = (file) => {
+  return new Promise((res, rej) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) {
+        rej('I could not read the product file');
+      } else {
+        res(data);
+      }
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((res, rej) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) rej('I could not write the product image to file');
+      res('SUCESS');
+    });
+  });
+};
+
+// This is a flat chain of promises
+readFilePro(`${__dirname}/product.txt`)
+  .then((res) => {
+    console.log('Product ID:', res);
+    return superagent.get(`https://fakestoreapi.com/products/${res}`);
+  })
+  .then((response) => {
+    console.log(response.body.image);
+    return writeFilePro(`${__dirname}/product-image.txt`, response.body.image);
+  })
+  .then(() => {
+    console.log('Product image saved to file');
+  })
+  .catch((err) => {
+    console.error('Error reading product file:', err);
+  });
