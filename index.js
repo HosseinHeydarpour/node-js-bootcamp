474580d1c29a5567b027a568da6f48afb199cd65
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-04 19:14:50 +0330

    From Callback Hell to Promises

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ fs.readFile(`${__dirname}/product.txt`, 'utf-8', (err, data) => {
   console.log('Product: ', data);
   superagent
     .get(`https://fakestoreapi.com/products/${data}`)
-    .end((err, res) => {
-      if (err) {
-        console.error('Error fetching product image:', err);
-        return;
-      }
+    .then((res) => {
       console.log(res.body.image);
+
       fs.writeFile(`${__dirname}/product-image.txt`, res.body.image, (err) => {
         if (err) {
           console.error('Error writing product image to file:', err);
@@ -19,5 +16,8 @@ fs.readFile(`${__dirname}/product.txt`, 'utf-8', (err, data) => {
           console.log('Product image saved to product-image.txt');
         }
       });
+    })
+    .catch((err) => {
+      console.error('Error writing product image to file:', err);
     });
 });
