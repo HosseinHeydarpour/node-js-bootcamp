d4234af85c6e5acf65991c4e1606f3730f1626e5
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-09 21:48:09 +0330

    Handling POST Requests

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 const app = express();
 
+// Middleware: if we disable this, req.body will be undefined
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   // res.status(200).send('Hello From the server!');
 //   res.status(200).json({
@@ -29,6 +32,32 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: 'error',
+          message: 'Could not save tour',
+        });
+      }
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
