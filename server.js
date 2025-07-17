514eda96e9423e6999eb9e0c6644dd7df9219bf1
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-17 11:22:27 +0330

    Creating a Simple Tour Model

--- server.js
+++ server.js
@@ -21,6 +21,24 @@ mongoose
   })
   .then(() => console.log('DB connection Successful'));
 
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'The tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'The tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', toursSchema);
+
 // console.log(app.get('env')); // We get development by default
 // console.log(process.env);
 
