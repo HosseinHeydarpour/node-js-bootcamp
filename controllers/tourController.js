77cb08868a849297512d49b5b077b3e4fc362d18
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-07-19 11:20:10 +0330

    Deleting Documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -90,13 +90,10 @@ exports.updateTour = async (req, res) => {
 
 exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndDelete(req.params.id);
-    res.status(200).json({
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
       status: 'success',
       message: 'Tour was deleted successfully',
-      data: {
-        tour,
-      },
     });
   } catch (error) {
     res.status(404).json({
