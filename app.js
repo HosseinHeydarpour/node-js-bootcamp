5a374200177c2f0616a0104cd3a3d1d3d983102d
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-10 15:10:21 +0330

    Catching Uncaught Exceptions

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   // Middleware to add request time
   req.requestTime = new Date().toISOString();
+  // console.log(x); throw error only when request happens
   next();
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXEPTION! Shutting down...');
+  console.log(err.name, err.message, '💚💛🧡');
+  // for uncaught exception we really really need to crash the app
+  process.exit(1);
+});
+
 // Because this is on the proccess and  the proccess is always the same for all of our js files we have access to this
 dotenv.config({
   path: './config.env',
@@ -28,7 +35,9 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message, '💚💛🧡');
   console.log('UNHANDLED REJECTION! Shutting down');
+  console.log(err.name, err.message, '💚💛🧡');
   server.close(() => process.exit(1));
 });
+
+// console.log(x);
