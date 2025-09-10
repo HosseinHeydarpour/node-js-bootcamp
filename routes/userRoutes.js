21fb197dcde754f96f027662d72b1226d23f1ecd
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-10 16:14:58 +0330

    Creating New Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 
+const authController = require('../controllers/authController');
+
+router.route('/signup').post(authController.signup);
+
 router
   .route('/')
   .get(userController.getAllUsers)
