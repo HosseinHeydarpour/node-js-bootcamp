5299d63813c5f34fb853ce7eeeaf68d82b178f9c
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-15 22:02:05 +0330

    Signing up Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,26 @@
+const jwt = require('jsonwebtoken');
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  //  Serious security flaw
+  // const newUser = await User.create(req.body);
+
+  // Use this instead
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token: token,
     data: {
       user: newUser,
     },
