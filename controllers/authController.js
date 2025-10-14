59f63ae18b4fd1a7df51937ab39dbb26b77819bb
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-10-14 16:43:06 +0330

    Sending JWT via Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,7 +12,21 @@ const signToken = (id) =>
   });
 
 const createAndSendToken = (user, statusCode, res) => {
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+
+    httpOnly: true,
+  };
   const token = signToken(user._id);
+
+  // Only for production
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  //  remove password from user object
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
