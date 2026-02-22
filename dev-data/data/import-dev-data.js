007d85629bc2fe98eb387ab4cc362e2894b2e0d3
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-02-22 18:32:08 +0330

    Importing Review and User Data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const fs = require('fs');
 const Tour = require('../../model/tourModel');
+const User = require('../../model/userModel');
+const Review = require('../../model/reviewModel');
 // Because this is on the proccess and  the proccess is always the same for all of our js files we have access to this
 dotenv.config({
   path: './config.env',
@@ -22,11 +24,17 @@ mongoose
   .then(() => console.log('DB connection Successful'));
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
 
 // Import data into db
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (error) {
     console.log(error);
@@ -38,6 +46,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany({});
+    await User.deleteMany({});
+    await Review.deleteMany({});
     console.log('Data successfully deleted!');
   } catch (error) {
     console.log(error);
